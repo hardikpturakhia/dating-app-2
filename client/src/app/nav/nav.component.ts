fb6c073091b56d632824041c0e357ce19e82592d
Hardik Turakhia <Hardik.tech1@outlook.com>
2021-05-26 16:52:53 +0530

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -24,10 +24,7 @@ export class NavComponent implements OnInit {
    return this.accountService.login(this.model).subscribe(response => {
      this.router.navigateByUrl('/members');
       console.log(response);
-      }, error =>{
-      console.error(error);
-      this.toastr.error(error.error);
-    });
+      });
       
   }
   logout() {
